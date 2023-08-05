18527cf4cd462bbc9a8336dca61428de95838759
badr ghanbi <bghanbi50@gmail.com>
2023-08-05 17:47:56 +0100

    create a custom dropdown

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
-const navbarToggle = document.querySelector(".navbar-toggle");
-const navbarLinks = document.querySelector(".navbar");
 
-navbarToggle.addEventListener("click", () => {
-    navbarLinks.classList.toggle("active");
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the dropdown element
+    const dropdown = document.querySelector('.dropdown');
+
+    // Get the dropdown span element (to toggle dropdown)
+    const dropdownLabel = dropdown.querySelector('span');
+
+    // Function to toggle the dropdown
+    function toggleDropdown() {
+        dropdown.classList.toggle('open');
+    }
+
+    // Add event listener to toggle the dropdown when the label is clicked
+    dropdownLabel.addEventListener('click', toggleDropdown);
+
+    // Get all the dropdown options (anchor elements) in an array
+    const dropdownOptions = Array.from(dropdown.querySelectorAll('li a'));
+
+    // Function to handle option selection
+    function handleOptionSelection(event) {
+        event.preventDefault(); // Prevent the default anchor link behavior
+        const selectedOptionText = this.textContent;
+        console.log(`Selected option: ${selectedOptionText}`);
+        toggleDropdown(); // Close the dropdown after selection (you can customize this behavior)
+    }
+
+    // Add event listeners to each option to handle selection
+    dropdownOptions.forEach(option => {
+        option.addEventListener('click', handleOptionSelection);
+    });
 });
