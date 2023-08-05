3ec35980b7ffe76689e7eec1103fc607bab322d3
badr ghanbi <bghanbi50@gmail.com>
2023-08-05 20:07:35 +0100

    creat a responsive navbar

--- main.js
+++ main.js
@@ -1,32 +1,24 @@
+const hamburgerMenu = document.querySelector('.hamburgerMenu')
+const closeIcon = document.querySelector('.closeIcon')
+const navList = document.querySelector('.nav__list')
 
-document.addEventListener('DOMContentLoaded', function () {
-    // Get the dropdown element
-    const dropdown = document.querySelector('.dropdown');
 
-    // Get the dropdown span element (to toggle dropdown)
-    const dropdownLabel = dropdown.querySelector('span');
+hamburgerMenu.addEventListener('click', showNavList)
+closeIcon.addEventListener('click', hideNavList)
 
-    // Function to toggle the dropdown
-    function toggleDropdown() {
-        dropdown.classList.toggle('open');
-    }
 
-    // Add event listener to toggle the dropdown when the label is clicked
-    dropdownLabel.addEventListener('click', toggleDropdown);
+console.log(hamburgerMenu)
 
-    // Get all the dropdown options (anchor elements) in an array
-    const dropdownOptions = Array.from(dropdown.querySelectorAll('li a'));
+function showNavList() {
+    navList.classList.remove('hidden')
+    closeIcon.classList.remove('hidden')
+    hamburgerMenu.classList.add('hidden')
+}
 
-    // Function to handle option selection
-    function handleOptionSelection(event) {
-        event.preventDefault(); // Prevent the default anchor link behavior
-        const selectedOptionText = this.textContent;
-        console.log(`Selected option: ${selectedOptionText}`);
-        toggleDropdown(); // Close the dropdown after selection (you can customize this behavior)
-    }
+function hideNavList() {
+    navList.classList.add('hidden')
+    closeIcon.classList.add('hidden')
+    hamburgerMenu.classList.remove('hidden')
+}
 
-    // Add event listeners to each option to handle selection
-    dropdownOptions.forEach(option => {
-        option.addEventListener('click', handleOptionSelection);
-    });
-});
+
